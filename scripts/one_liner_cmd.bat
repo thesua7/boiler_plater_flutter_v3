d6f52db8377a6de088b15afc7b1b67aaff7acb13
thesua7 <thesua711@gmail.com>
2025-09-27 15:40:37 +0600

    unused files are removedd

--- scripts/one_liner_cmd.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-curl -L -o "%TEMP%\standalone_generator.bat" "https://raw.githubusercontent.com/thesua7/boiler_plater_flutter_v3/master/scripts/standalone_generator.bat" && call "%TEMP%\standalone_generator.bat" && del "%TEMP%\standalone_generator.bat"
